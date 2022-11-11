35d851cf7be5836cf09cf2882a407cea05bb2a37
HitYes <1321649@senati.pe>
2022-11-11 17:11:35 -0500

    Importando base de datos

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/listar', function () {
+    return view('listar');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
